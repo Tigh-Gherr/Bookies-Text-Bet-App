5be7a60254ca3c177fc7b9a0ddb48f2c1e0c5b80
Tigh-Gherr <tigtigertig@gmail.com>
2015-12-12 19:14:06 +0000

    Commented ParseUtils.java

--- app/src/main/java/com/tommyfrenchbookmakers/officialapp/customutils/ParseUtils.java
+++ app/src/main/java/com/tommyfrenchbookmakers/officialapp/customutils/ParseUtils.java
@@ -1,7 +1,6 @@
 package com.tommyfrenchbookmakers.officialapp.customutils;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.tommyfrenchbookmakers.officialapp.docketobjects.BetSelections;
 import com.tommyfrenchbookmakers.officialapp.docketobjects.BetWagers;
@@ -38,19 +37,23 @@ import java.util.Date;
 /**
  * Created by Tíghearnán on 31/08/2015.
  */
-public class ParseUtils {
+public final class ParseUtils {
 
+    // Used to download JSON data from webservice and parse it to a String.
     public static String dataFromWebService(Context c, String dataToParse) {
         String dataFromWebService = "";
         HttpURLConnection connection = null;
         BufferedReader reader = null;
 
+        // Download.
         try {
+            // Create URL and open connection.
             URL url = new URL(dataToParse);
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
             connection.connect();
 
+            // Create input and output streams.
             InputStream inputStream = connection.getInputStream();
             if (inputStream == null) return null;
 
@@ -78,12 +81,14 @@ public class ParseUtils {
         return dataFromWebService;
     }
 
+    // Used to create a Docket object from JSON data provided from a String.
     public static boolean docketFromJSON(Context c, String dataToParse) {
         Docket docket;
         try {
             JSONObject jsonObject = new JSONObject(dataToParse);
             JSONObject jsonDocket = jsonObject.getJSONObject("docket");
 
+            // Get information from downloaded JSON object and store it in relevant variables.
             int docketNumber = jsonDocket.getInt("docket_number");
             String accountNumber = jsonDocket.getString("account");
             String barcode = jsonDocket.getString("barcode");
@@ -91,28 +96,38 @@ public class ParseUtils {
             double totalPayout = jsonDocket.getDouble("total_payout");
             boolean winner = jsonDocket.getInt("winner") == 1;
 
+            // Instantiate docket object with previously extracted data.
             docket = new Docket(docketNumber, accountNumber, barcode, totalStake, totalPayout, winner);
 
+            // Move onto the individual bets in the docket.
             JSONArray jsonArrayBets = jsonDocket.getJSONArray("bets");
             int betLength = jsonArrayBets.length();
+            // For each bet in the docket.
             for (int currentBet = 0; currentBet < betLength; currentBet++) {
                 JSONObject jsonBets = jsonArrayBets.getJSONObject(currentBet);
+
+                // Get information from downloaded JSON object and store it in relevant variables.
                 int betNumber = jsonBets.getInt("bet_number");
                 int betDocketNumber = jsonBets.getInt("docket_number");
                 double betStake = jsonBets.getDouble("bet_stake");
                 double betPayout = jsonBets.getDouble("bet_payout");
                 boolean betWinner = jsonBets.getInt("winner") == 1;
 
+                // Instantiate bet object with previously extracted data and add it to the docket.
                 docket.getBets().add(new DocketBet(betNumber,
                         betDocketNumber,
                         betStake,
                         betPayout,
                         betWinner));
 
+                // Move onto individual selections in the bet.
                 JSONArray jsonArraySelections = jsonBets.getJSONArray("selections");
                 int selectionLength = jsonArraySelections.length();
+                // For every selection in the bet.
                 for (int currentSelection = 0; currentSelection < selectionLength; currentSelection++) {
                     JSONObject jsonSelections = jsonArraySelections.getJSONObject(currentSelection);
+
+                    // Get information from downloaded JSON object and store it in relevant variables.
                     int selectionBetNumber = jsonSelections.getInt("bet_number");
                     String name = jsonSelections.getString("name");
                     String odds = jsonSelections.getString("odds");
@@ -122,17 +137,19 @@ public class ParseUtils {
                             Sport.getSport(jsonSelections.getString("sport")) : Sport.UNKNOWN;
                     int deadHeat = jsonSelections.getInt("dead_heat");
 
+                    // Instantiate Selection object with previously extracted data and add it to the bet.
                     docket.getBets().get(currentBet).getSelections()
                             .add(new BetSelections(selectionBetNumber, name, odds, position, sport, rule4, deadHeat));
-
-                    Log.v("ParseUtils", docket.getBets().get(currentBet)
-                            .getSelections().get(currentSelection).toString());
                 }
 
+                // Move onto individual wagers in the bet.
                 JSONArray jsonArrayWagers = jsonBets.getJSONArray("wagers");
                 int wagerLength = jsonArrayWagers.length();
+                // For each wager in the bet.
                 for (int currentWager = 0; currentWager < wagerLength; currentWager++) {
                     JSONObject jsonWagers = jsonArrayWagers.getJSONObject(currentWager);
+
+                    // Get information from downloaded JSON object and store it in relevant variables.
                     int wagerNumber = jsonWagers.getInt("wager_number");
                     int wagerBetNumber = jsonWagers.getInt("bet_number");
                     double unitStake = jsonWagers.getDouble("unit_stake");
@@ -141,6 +158,7 @@ public class ParseUtils {
                     boolean wagerWinner = jsonWagers.getInt("winner") == 1;
                     double payout = jsonWagers.getDouble("payout");
 
+                    // Instantiate Wager object from previously extracted information and add it to the bet.
                     docket.getBets().get(currentBet).getWagers()
                             .add(new BetWagers(wagerNumber,
                                     wagerBetNumber,
@@ -149,26 +167,23 @@ public class ParseUtils {
                                     wagerType,
                                     wagerWinner,
                                     payout));
-
-                    Log.v("ParseUtils",
-                            docket.getBets().get(currentBet)
-                                    .getWagers().get(currentWager).toString());
                 }
-
-                Log.v("ParseUtils", docket.getBets().get(currentBet).toString());
             }
         } catch (JSONException e) {
             e.printStackTrace();
             return false;
         }
 
+        // Save downloaded Docket to singleton.
         GlobalDocket.get(c).setDocket(docket);
         return true;
     }
 
+    // Used to parse Meeting and Market objects from downloaded XML data.
     public static boolean meetingsAndMarketsFromXML(Context c, String dataToParse) throws XmlPullParserException, IOException, ParseException {
         ArrayList<Meeting> meetings = new ArrayList<>();
 
+        // Setup the XML parser.
         XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
         XmlPullParser parser = factory.newPullParser();
         parser.setInput(new StringReader(dataToParse));
@@ -176,21 +191,30 @@ public class ParseUtils {
         final String XML_MEETING = "type";
         final String XML_MARKET = "market";
 
+
         Meeting meeting = null;
         ArrayList<Market> markets = new ArrayList<>();
+        // Used to keep track of what part of the XML data the parser is at.
         int eventType = parser.next();
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
+        // WHile not at the end of the XML document.
         while (eventType != XmlPullParser.END_DOCUMENT) {
-            if(eventType == XmlPullParser.START_TAG) {
-                if(parser.getName().equals(XML_MEETING)) {
+            // If the parser is at a start tag.
+            if (eventType == XmlPullParser.START_TAG) {
+                // If the parser is at a Meeting.
+                if (parser.getName().equals(XML_MEETING)) {
+                    // Get information from downloaded XML document and store it in relevant variables.
                     String id = parser.getAttributeValue(null, "id");
                     String name = parser.getAttributeValue(null, "name");
                     String lastUpdate = parser.getAttributeValue(null, "lastUpdateTime");
 
+                    // Create Meeting object from previously extracted data.
                     meeting = new Meeting(name, id, lastUpdate);
-                } else if(parser.getName().equals(XML_MARKET)) {
+                // If the parser is at a market.
+                } else if (parser.getName().equals(XML_MARKET)) {
+                    // Get information from downloaded XML document and store it in relevant variables.
                     String id = parser.getAttributeValue(null, "id");
                     String name = parser.getAttributeValue(null, "name");
                     Date raceDate = dateFormat.parse(parser.getAttributeValue(null, "date"));
@@ -201,13 +225,19 @@ public class ParseUtils {
                     boolean tricast = parser.getAttributeValue(null, "tricastAvailable").equals("Y");
 
                     Date todayDate = dateFormat.parse(dateFormat.format(new Date()));
-                    if(todayDate.equals(raceDate) && name.endsWith(" - Win")) {
+                    // If the race is today.
+                    if (todayDate.equals(raceDate) && name.endsWith(" - Win")) {
+                        // Create Market  object from previously extracted data and add it to the List<> of Markets.
                         markets.add(new Market(name, offTime, id, ewOdds, ewPlaces, raceDate, tricast));
                     }
                 }
-            } else if(eventType == XmlPullParser.END_TAG) {
-                if(parser.getName().equals(XML_MEETING)) {
-                    if(markets.size() > 0) {
+            // If the parse is at an end tag.
+            } else if (eventType == XmlPullParser.END_TAG) {
+                // If the parser is leaving a Meeting.
+                if (parser.getName().equals(XML_MEETING)) {
+                    // If there are any Markets in the current Meeting that are on today.
+                    if (markets.size() > 0) {
+                        // Save the Markets to the Meeting object and add the Meeting to the List<>.
                         meeting.setMarkets(markets);
                         meetings.add(meeting);
                     }
@@ -215,15 +245,19 @@ public class ParseUtils {
                 }
             }
 
+            // Move the parser onto the next section.
             eventType = parser.next();
         }
 
+        // Save the downloaded Meetings to a singleton.
         MeetingsSingleton.get(c).setMeetings(meetings);
 
         return true;
     }
 
+    // Used to create Participant objects for a provided Market, using a downloaded XML document.
     public static boolean participantFromMarketFromXML(Context c, String dataToParse, Market market) throws XmlPullParserException, IOException {
+        // Create instance of XML parser.
         XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
         XmlPullParser parser = factory.newPullParser();
         parser.setInput(new StringReader(dataToParse));
@@ -232,17 +266,24 @@ public class ParseUtils {
         final String XML_PARTICIPANT = "participant";
         Participant participant = null;
         boolean participantsMatchMeetingId = false;
+
+        // Keeps track of what part of the XML document the parser is at.
         int eventType = parser.next();
 
+        // While not at the end of the XML document.
         while (eventType != XmlPullParser.END_DOCUMENT) {
 
-            if(eventType == XmlPullParser.START_TAG && parser.getName().equals(XML_MARKET)) {
+            // If the parser is at the start of a Market.
+            if (eventType == XmlPullParser.START_TAG && parser.getName().equals(XML_MARKET)) {
+                // If the current Markets ID equals the passed Markets ID.
                 participantsMatchMeetingId = parser.getAttributeValue(null, "id").equals(market.getId());
+            // If the parser is leaving a Market.
             } else if (eventType == XmlPullParser.END_TAG && parser.getName().equals(XML_MARKET)) {
+                // Sort all the downloaded participants by odds.
                 Collections.sort(market.getParticipants(), new Comparator<Participant>() {
                     @Override
                     public int compare(Participant lhs, Participant rhs) {
-                        if(lhs.getOddsDecimal().equals("SP") || rhs.getOddsDecimal().equals("SP")) {
+                        if (lhs.getOddsDecimal().equals("SP") || rhs.getOddsDecimal().equals("SP")) {
                             return lhs.getName().compareToIgnoreCase(rhs.getName());
                         } else {
                             return Double.compare(Double.parseDouble(lhs.getOddsDecimal()), Double.parseDouble(rhs.getOddsDecimal()));
@@ -251,30 +292,40 @@ public class ParseUtils {
                 });
             }
 
-            if(participantsMatchMeetingId) {
-                if(eventType == XmlPullParser.START_TAG && parser.getName().equals(XML_PARTICIPANT)) {
+            // If the current Market ID matches the passed Markets ID.
+            if (participantsMatchMeetingId) {
+                // If the pare is at the start of a Participant.
+                if (eventType == XmlPullParser.START_TAG && parser.getName().equals(XML_PARTICIPANT)) {
+                    // Get information from downloaded XML document and store it in relevant variables.
                     String name = parser.getAttributeValue(null, "name");
                     String id = parser.getAttributeValue(null, "id");
                     String odds = parser.getAttributeValue(null, "odds");
 
                     String decimalOdds;
-                    if(parser.getAttributeValue(null, "oddsDecimal").equals("SP")) {
-                        if(name.equals("Favourite")) decimalOdds = "0.25";
-                        else if(name.equals("2nd Favourite")) decimalOdds = "0.5";
+                    // If a selections odds are SP.
+                    if (parser.getAttributeValue(null, "oddsDecimal").equals("SP")) {
+                        if (name.equals("Favourite")) decimalOdds = "0.25";
+                        else if (name.equals("2nd Favourite")) decimalOdds = "0.5";
                         else decimalOdds = "1";
                     } else {
                         String[] absOdds = odds.split("/");
-                        decimalOdds = ((Double.parseDouble(absOdds[0]) / Double.parseDouble(absOdds[1])) + 1)+"";
+                        decimalOdds = ((Double.parseDouble(absOdds[0]) / Double.parseDouble(absOdds[1])) + 1) + "";
                     }
 
+                    // Create Participant object from previously extracted information.
                     participant = new Participant(id, name, odds, decimalOdds);
+
+                    // Give the Participant some information about the meeting it is in.
                     participant.setMarketId(market.getId());
                     participant.setMarketName(market.getName().substring(0, market.getName().lastIndexOf(" - ")));
+                // If the parser is leaving a Participant.
                 } else if (eventType == XmlPullParser.END_TAG && parser.getName().equals(XML_PARTICIPANT)) {
+                    // Attach the Pariticpant oject to the passed Market object.
                     market.getParticipants().add(participant);
                 }
             }
 
+            // Move the parser to the next part of the XML document.
             eventType = parser.next();
         }
 
