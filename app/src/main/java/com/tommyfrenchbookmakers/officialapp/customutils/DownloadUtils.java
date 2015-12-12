ee21f0497034d3c572708d8a308b001fa701e31e
Tigh-Gherr <tigtigertig@gmail.com>
2015-12-12 18:37:25 +0000

    Commented DownloadUtils.java

--- app/src/main/java/com/tommyfrenchbookmakers/officialapp/customutils/DownloadUtils.java
+++ app/src/main/java/com/tommyfrenchbookmakers/officialapp/customutils/DownloadUtils.java
@@ -6,52 +6,45 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.support.v4.app.NavUtils;
-import android.util.Log;
 
-import com.android.tighearnan.frenchsscanner.R;
 import com.tommyfrenchbookmakers.officialapp.interfaces.DataDownloadListener;
 import com.tommyfrenchbookmakers.officialapp.meetingobjects.Market;
-import com.tommyfrenchbookmakers.officialapp.meetingobjects.Meeting;
-import com.tommyfrenchbookmakers.officialapp.meetingobjects.Participant;
-import com.tommyfrenchbookmakers.officialapp.singletons.MeetingsSingleton;
 
-import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
 
 /**
  * Created by Tíghearnán on 02/09/2015.
  */
 public class DownloadUtils {
 
+    // Instantiated for downloading a docket when with a barcode.
     public static class DocketFromBarcode extends AsyncTask<String, Void, Boolean> {
 
+        // Member variables.
         private Context mContext;
         private String mDownloadedData;
         private ProgressDialog mDownloadingDocketDialog;
         private DataDownloadListener mStatusListener;
 
+        // Constructor.
         public DocketFromBarcode(Context c, DataDownloadListener listener) {
             mContext = c;
             mStatusListener = listener;
         }
 
+        // Before the download.
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            // TODO: Remove ProgressDialog, use ProgressBar and DataDownloadListener.
+            // Create and display the ProgressDialog
             mDownloadedData = "";
             mDownloadingDocketDialog = new ProgressDialog(mContext);
             mDownloadingDocketDialog.setMessage("Downloading Docket...");
@@ -72,41 +65,53 @@ public class DownloadUtils {
             mDownloadingDocketDialog.show();
         }
 
+        // Executed in separate thread.
         @Override
         protected Boolean doInBackground(String... params) {
             if (params == null) return null;
 
+            // Download and parse the information to a string.
             mDownloadedData = ParseUtils.dataFromWebService(mContext, params[0]);
+            // Parse the string into a Docket object and return boolean for success.
             return ParseUtils.docketFromJSON(mContext, mDownloadedData);
         }
 
+        // After the download.
         @Override
         protected void onPostExecute(Boolean success) {
             super.onPostExecute(success);
 
+            // TODO: Call to onDownloadComplete will instead dismiss the ProgressDialog and setup the host Fragment.
+            // Dismiss the ProgressDialog. Send downloaded data and success information back to Fragment.
             mDownloadingDocketDialog.dismiss();
             mStatusListener.onDownloadComplete(success, mDownloadedData);
         }
     }
 
+    // Instantiated for downloading a docket with an account number and reference number.
     public static class DocketFromAccountAndReference extends AsyncTask<String, Void, Boolean> {
 
+        // Member variables.
         private Context mContext;
         private DataDownloadListener mStatus;
         private String mDownloadedData;
         private ProgressDialog mDownloadingProgressDialog;
 
+        // Constructor.
         public DocketFromAccountAndReference(Context c, DataDownloadListener listener) {
             mContext = c;
             mStatus = listener;
         }
 
+        // Before the download.
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
 
             mDownloadedData = "";
 
+            // TODO: Remove ProgressDialog, use ProgressBar and DataDownloadListener.
+            // Setup and display the ProgressDialog
             mDownloadingProgressDialog = new ProgressDialog(mContext);
             mDownloadingProgressDialog.setMessage("Downloading docket...");
             mDownloadingProgressDialog.setProgressPercentFormat(null);
@@ -125,35 +130,45 @@ public class DownloadUtils {
             mDownloadingProgressDialog.show();
         }
 
+        // Executed in a separate thread.
         @Override
         protected Boolean doInBackground(String... params) {
             if (params == null) return null;
-            mDownloadedData = ParseUtils.dataFromWebService(mContext, params[0]);
 
+            // Download and parse the information to a string.
+            mDownloadedData = ParseUtils.dataFromWebService(mContext, params[0]);
+            // Parse the string into a Docket object and return boolean for success.
             return ParseUtils.docketFromJSON(mContext, mDownloadedData);
         }
 
+        // After the download.
         @Override
         protected void onPostExecute(Boolean success) {
             super.onPostExecute(success);
 
+            // TODO: Call to onDownloadComplete will instead dismiss the ProgressDialog and setup the host Fragment.
+            // Dismiss the ProgressDialog. Send downloaded data and success information back to Fragment.
             mDownloadingProgressDialog.dismiss();
             mStatus.onDownloadComplete(success, mDownloadedData);
         }
     }
 
+    // Instantiated when downloading Horse Racing information.
     public static class WilliamHillBetting extends AsyncTask<String, Void, Boolean> {
 
+        // Member variables.
         private Market mMarket;
         private Context mContext;
         private DataDownloadListener mDownloadListener;
 
+        // Constructor.
         public WilliamHillBetting(Context context, DataDownloadListener listener) {
             mContext = context;
             mDownloadListener = listener;
             mMarket = null;
         }
 
+        // Constructor.
         public WilliamHillBetting(Context context, DataDownloadListener listener, Market market) {
             mContext = context;
             mDownloadListener = listener;
@@ -161,26 +176,35 @@ public class DownloadUtils {
         }
 
 
+        // Before the download.
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            // Notify host fragment that download is starting.
             mDownloadListener.onDownloadStart();
         }
 
+        // Executed in a separate thread.
         @Override
         protected Boolean doInBackground(String... params) {
+            // Assume success, wait to be falsified.
             boolean success = true;
             HttpURLConnection connection;
 
+            // Download.
             try {
+                // Get URL and open connection with it.
                 URL url = new URL(params[0]);
                 connection = (HttpURLConnection) url.openConnection();
 
+                // Prepare Streams for reading and writing.
                 ByteArrayOutputStream out = new ByteArrayOutputStream();
                 InputStream inputStream = connection.getInputStream();
 
+                // If the response code anything other than ok, return an error.
                 if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) return null;
 
+                // Read the downloaded data.
                 int bytesRead = 0;
                 byte[] buffer = new byte[1024];
                 while ((bytesRead = inputStream.read(buffer)) > 0) {
@@ -188,27 +212,31 @@ public class DownloadUtils {
                 }
                 out.close();
 
+                // Parse the downloaded data to a String.
                 String xmlData = new String(out.toByteArray());
 
+                // If no Market was passed, we are downloading the Meetings and Markets.
                 if (mMarket == null) {
                     ParseUtils.meetingsAndMarketsFromXML(mContext, xmlData);
+                // If a Market was passed, we are downloading the participants.
                 } else {
                     ParseUtils.participantFromMarketFromXML(mContext, xmlData, mMarket);
                 }
 
-            } catch (IOException e) {
+            } catch (IOException | XmlPullParserException | ParseException e) {
                 e.printStackTrace();
+                // Return false if error was thrown.
                 success = false;
-            } catch (XmlPullParserException | ParseException e) {
-                e.printStackTrace();
             }
 
             return success;
         }
 
+        // After the download.
         @Override
         protected void onPostExecute(Boolean aBoolean) {
             super.onPostExecute(aBoolean);
+            // Notify the host fragment the download is completed, and send the whether or not it was successful.
             mDownloadListener.onDownloadComplete(aBoolean, "");
         }
 
